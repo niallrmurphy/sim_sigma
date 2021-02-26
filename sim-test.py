0711a87477c1f4bb42186ffdf539606859c373bf
Niall R Murphy <niallm@gmail.com>
2021-02-26 12:10:50 +0000

    Add different distributions, expand test suite

--- sim-test.py
+++ sim-test.py
@@ -12,9 +12,13 @@ from unittest.mock import patch
 
 class TestSimMethods(unittest.TestCase):
   def setUp(self):
-    self.ms = sim.microservice('normal', 100, 30)
-    self.ms2 = sim.microservice('normal', 100, 30)
-    self.ms3 = sim.microservice('normal', 100, 30)
+    self.BIG_ENOUGH_SAMPLES = 5000
+    self.RNG_SEED = 220806
+    self.ms = sim.microservice('normal', self.RNG_SEED, 100, 30)
+    self.ms2 = sim.microservice('normal', self.RNG_SEED, 100, 30)
+    self.ms3 = sim.microservice('normal', self.RNG_SEED, 100, 30)
+    self.ms4 = sim.microservice('gamma', self.RNG_SEED, 100, 2)
+    self.ms5 = sim.microservice('exponential', self.RNG_SEED, 100, 0)
 
   def test_unitary_static_micro_latency(self):
     with patch.object(sim.microservice, 'return_self_latency', return_value=100):
@@ -59,11 +63,125 @@ class TestSimMethods(unittest.TestCase):
     self.assertGreater(self.ms.calculate_latency(), 0)
 
   def test_plot_distrib(self):
-    for x in range(1,2500):
+    """Enough samples gets you a normal distribution."""
+    for x in range(1,self.BIG_ENOUGH_SAMPLES):
       self.ms.calculate_latency()
     mydata = pd.Series(self.ms.latency_history)
     self.assertGreater(shapiro(mydata).pvalue, 0.05)
 
-          
+  def test_gamma_distrib(self):
+    """A gamma distribution on its own is not normally distributed."""
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms4.calculate_latency()
+    mydata = pd.Series(self.ms4.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)
+
+  def test_gamma_dependent(self):
+    """A gamma distribution as a serial dependent of a normal distribution
+       is mostly normally distributed."""
+    self.ms.add_serial_dependency(self.ms4)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    # self.assertGreater(shapiro(mydata).pvalue, 0.05)
+
+  def test_bestof_gamma(self):
+    """A gamma distribution as a subcomponent in 'bestof' is mostly normally distributed"""
+    self.ms.add_bestof_dependency(self.ms2)
+    self.ms.add_bestof_dependency(self.ms4)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertGreater(shapiro(mydata).pvalue, 0.05)
+
+  def test_worstof_gamma(self):
+    """A gamma distribution as a subcomponent in 'worstof' is mostly normally distributed"""
+    self.ms.add_worstof_dependency(self.ms2)
+    self.ms.add_worstof_dependency(self.ms4)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertGreater(shapiro(mydata).pvalue, 0.05)
+
+  def test_exponential_distrib(self):
+    """An exponential distribution on its own is not normally distributed."""
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms5.calculate_latency()
+    mydata = pd.Series(self.ms5.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)
+
+  def test_exponential_dependent(self):
+    """An exponential distribution as a serial dependent of a normal distribution
+       is not normally distributed."""
+    self.ms.add_serial_dependency(self.ms5)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)  
+
+  def test_bestof_exponential(self):
+    """An exponential distribution as a subcomponent in 'bestof' is not normally distributed"""
+    self.ms.add_bestof_dependency(self.ms2)
+    self.ms.add_bestof_dependency(self.ms5)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)
+
+  def test_worstof_exponential(self):
+    """An exponential distribution as a subcomponent in 'worstof' is not normally distributed"""
+    self.ms.add_worstof_dependency(self.ms2)
+    self.ms.add_worstof_dependency(self.ms5)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)
+
+  def test_find_non_normal_bestof(self):
+    """We can find a non-normal distribution in 'bestof' subcomponents'"""
+    self.ms.add_bestof_dependency(self.ms2)
+    self.ms.add_bestof_dependency(self.ms3)
+    self.ms.add_bestof_dependency(self.ms5)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata1 = pd.Series(self.ms.latency_history)
+    mydata2 = pd.Series(self.ms2.latency_history)
+    mydata3 = pd.Series(self.ms3.latency_history)
+    mydata5 = pd.Series(self.ms5.latency_history)
+    # We expect the root node (self.ms) and self.ms5 NOT be normally
+    # distributed, and ms2 & ms3 to be.
+    self.assertLess(shapiro(mydata1).pvalue, 0.05)
+    self.assertLess(shapiro(mydata5).pvalue, 0.05)
+    self.assertGreater(shapiro(mydata2).pvalue, 0.05)
+    self.assertGreater(shapiro(mydata3).pvalue, 0.05)
+
+  def test_find_non_normal_worstof(self):
+    """We can find a non-normal distribution in 'worstof' subcomponents'"""
+    self.ms.add_worstof_dependency(self.ms2)
+    self.ms.add_worstof_dependency(self.ms3)
+    self.ms.add_worstof_dependency(self.ms5)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata1 = pd.Series(self.ms.latency_history)
+    mydata2 = pd.Series(self.ms2.latency_history)
+    mydata3 = pd.Series(self.ms3.latency_history)
+    mydata5 = pd.Series(self.ms5.latency_history)
+    # We expect the root node (self.ms) and self.ms5 NOT be normally
+    # distributed, and ms2 & ms3 to be.
+    self.assertLess(shapiro(mydata1).pvalue, 0.05)
+    self.assertLess(shapiro(mydata5).pvalue, 0.05)
+    self.assertGreater(shapiro(mydata2).pvalue, 0.05)
+    self.assertGreater(shapiro(mydata3).pvalue, 0.05)
+
+  def test_exponential_longchain_dependent(self):
+    """A non-normally distributed system in a chain is non-normally distributed at root."""
+    self.ms.add_serial_dependency(self.ms2)
+    self.ms2.add_serial_dependency(self.ms5)
+    self.ms5.add_serial_dependency(self.ms3)
+    for x in range(1, self.BIG_ENOUGH_SAMPLES):
+      self.ms.calculate_latency()
+    mydata = pd.Series(self.ms.latency_history)
+    self.assertLess(shapiro(mydata).pvalue, 0.05)  
+    
 if __name__ == '__main__':
     unittest.main()
--- sim.py
+++ sim.py
@@ -7,6 +7,83 @@ import numpy as np
 import random
 import simpy
 
+from numpy.random import default_rng
+from scipy.stats import norm
+from scipy.stats import shapiro
+
+
+class microservice:
+  DISTRIBUTIONS = ['normal', 'gamma', 'exponential']
+  MAX_LATENCY = 1000000
+  def __init__(self, distribution_type="normal", seed=None, central=100, stddev=30):
+    if distribution_type not in microservice.DISTRIBUTIONS:
+      raise Exception("Distribution type %s unknown, no sensible way to proceed",
+        distribution_type)
+    else:
+      self.distribution_type = distribution_type
+      if seed == None:
+        self.rng = default_rng()
+      else:
+        self.rng = default_rng(seed)
+    self.dependencies = []
+    self.bestof_dependencies = []
+    self.worstof_dependencies = []
+    self.last_latency = 0
+    self.latency_history = []
+    self.central = central
+    self.stddev = stddev
+
+  def add_serial_dependency(self, thing):
+    self.dependencies.append(thing)
+
+  def add_bestof_dependency(self, thing):
+    self.bestof_dependencies.append(thing)
+
+  def add_worstof_dependency(self, thing):
+    self.worstof_dependencies.append(thing)
+
+  def return_self_latency(self):
+    if self.distribution_type == "normal":
+      return self.rng.normal(self.central, self.stddev)
+    elif self.distribution_type == "gamma":
+      return self.rng.gamma(self.central, self.stddev)
+    elif self.distribution_type == "exponential":
+      return self.rng.exponential(self.central)
+    else:
+      return 1 # This is bad
+
+  def calculate_latency(self):
+    self.last_latency = 0
+    # Calculate serialised latency for explicit dependencies, if any
+    for x in self.dependencies:
+      self.last_latency += x.calculate_latency()
+    # Calculate "best of" latency for explicit dependencies, i.e. LB
+    group_best_latency = microservice.MAX_LATENCY
+    for x in self.bestof_dependencies:
+      _ = x.calculate_latency()
+      if _ < group_best_latency:
+        group_best_latency = _
+    if group_best_latency != microservice.MAX_LATENCY:
+      self.last_latency += group_best_latency
+    # Calculate "worst of" latency for explicit dependencies, i.e. storage
+    group_worst_latency = 0
+    for x in self.worstof_dependencies:
+      _ = x.calculate_latency()
+      if _ > group_worst_latency:
+        group_worst_latency = _
+    self.last_latency += group_worst_latency
+    # Self latency
+    self.last_latency += self.return_self_latency()
+    self.latency_history.append(self.last_latency)
+    return self.last_latency
+
+  def is_normally_distributed(self):
+    mydata = pd.Series(self.latency_history)
+    if (shapiro(mydata).pvalue > 0.05):
+      return True
+    else:
+      return False
+
 class macroservice:
   def __init__(self, node_size = 10, probability = 0.3, k = 3, until=10):
     #self.graph = networkx.generators.random_graphs.erdos_renyi_graph(
@@ -36,58 +113,6 @@ class macroservice:
     nx.draw_networkx(self.graph, pos)
     plt.title("Random Graph Generation Example")
     plt.show()
-
-class microservice:
-  DISTRIBUTIONS = ['normal']
-  MAX_LATENCY = 1000000
-  def __init__(self, distribution_type="normal", central=100, stddev=30):
-    if distribution_type not in microservice.DISTRIBUTIONS:
-      raise Exception("Distribution type %s unknown, no sensible way to proceed",
-        distribution_type)
-    self.dependencies = []
-    self.bestof_dependencies = []
-    self.worstof_dependencies = []
-    self.last_latency = 0
-    self.latency_history = []
-    self.central = central
-    self.stddev = stddev
-
-  def add_serial_dependency(self, thing):
-    self.dependencies.append(thing)
-
-  def add_bestof_dependency(self, thing):
-    self.bestof_dependencies.append(thing)
-
-  def add_worstof_dependency(self, thing):
-    self.worstof_dependencies.append(thing)
-
-  def return_self_latency(self):
-    return np.random.normal(self.central, self.stddev)
-
-  def calculate_latency(self):
-    self.last_latency = 0
-    # Calculate serialised latency for explicit dependencies, if any
-    for x in self.dependencies:
-      self.last_latency += x.calculate_latency()
-    # Calculate "best of" latency for explicit dependencies, i.e. LB
-    group_best_latency = microservice.MAX_LATENCY
-    for x in self.bestof_dependencies:
-      _ = x.calculate_latency()
-      if _ < group_best_latency:
-        group_best_latency = _
-    if group_best_latency != microservice.MAX_LATENCY:
-      self.last_latency += group_best_latency
-    # Calculate "worst of" latency for explicit dependencies, i.e. storage
-    group_worst_latency = 0
-    for x in self.worstof_dependencies:
-      _ = x.calculate_latency()
-      if _ > group_worst_latency:
-        group_worst_latency = _
-    self.last_latency += group_worst_latency
-    # Self latency
-    self.last_latency += self.return_self_latency()
-    self.latency_history.append(self.last_latency)
-    return self.last_latency
 
 def main():
   ms = macroservice(20, 0.3, 3, 10)
